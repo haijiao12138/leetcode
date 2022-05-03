37f35a743cab3d902d0bdd01b721ebe6533d76ee
houyuanbo <houyuanbo@qianxin.com>
2022-05-03 16:31:11 +0800

    力扣刷题0503

---
+++ src/main/java/com/haijiao12138/demo/leetcode/code2022/五月/day_0503/力扣76_最小覆盖字串_困难.java
@@ -0,0 +1,46 @@
+package com.haijiao12138.demo.leetcode.code2022.五月.day_0503;
+
+/**
+ * @ClassName 力扣76_最小覆盖字串_困难
+ * @Author houyuanbo
+ * @Date 2022/5/3 13:58
+ * @Description TODO
+ * @Version 滑动窗口
+ **/
+public class 力扣76_最小覆盖字串_困难 {
+    public static void main(String[] args) {
+    String s = "ADOBECODEBANC";
+    String t = "ABC";
+    String s1 = minWindow(s, t);
+        System.out.println(s1);
+    }
+    public static String minWindow(String s,String t){
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        int n = s1.length;
+        int m = t1.length;
+        int[] arr = new int[58];//统计字符个数
+        int f = 0,w = 0,num = 0;//快慢指针 记录符合的字符个数
+        String size = "";
+        for (int i = 0; i < m; i++) {
+            arr[t1[i]-'A']--;
+        }
+        for ( f = 0; f < n; f++) {
+            arr[s1[f] - 'A']++;
+            if (arr[s1[f] - 'A']<=0){
+                num++;//记录符合目标字符的个数
+            }
+            if (num == m){//目标字符的个数等于t字符串的长度 开始慢指针遍历
+                while (arr[s1[w] - 'A'] > 0){
+                    //不是目标字符 因为目标字符都小于等于0
+                    arr[s1[w]-'A']--;
+                    w++;
+                }
+                if (""==size || size.length() > f-w +1){
+                    size = s.substring(w,f+1);
+                }
+            }
+        }
+        return size;
+    }
+}
---
+++ src/main/java/com/haijiao12138/demo/leetcode/code2022/五月/day_0503/力扣79_单词搜索_中等.java
@@ -0,0 +1,42 @@
+package com.haijiao12138.demo.leetcode.code2022.五月.day_0503;
+
+/**
+ * @ClassName 力扣79_单词搜索_中等
+ * @Author houyuanbo
+ * @Date 2022/5/3 15:45
+ * @Description TODO
+ * @Version 回溯
+ **/
+public class 力扣79_单词搜索_中等 {
+    public static void main(String[] args) {
+        char[][] board ={{'A','B','C','E'},{'S','F','C','S'}, {'A','D','E','E'}};
+        String word = "ABCCED";
+        boolean exist = exist(board, word);
+        System.out.println(exist);
+    }
+
+    public static boolean exist(char[][] board,String word){
+        for (int i = 0; i < board.length; i++) {//行
+            for (int j = 0; j < board[0].length; j++) {//列
+                if (search(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean search(char[][] board,String word,int i,int j,int k){
+        if (k >= word.length()){//当长度大于要匹配大长度后 认为存在
+            return true;
+        }
+        if (i<0 || i>=board.length || j<0 ||j>=board[0].length || board[i][j] != word.charAt(k)){
+            return false;
+        }
+        board[i][j] += 256;
+        boolean result = search(board, word, i, j + 1, k + 1) || search(board,word,i-1,j,k+1) || search(board,word,i+1,j,k+1) || search(board,word,i,j-1,k+1);
+        board[i][j] -= 256;
+        return result;
+
+    }
+}
