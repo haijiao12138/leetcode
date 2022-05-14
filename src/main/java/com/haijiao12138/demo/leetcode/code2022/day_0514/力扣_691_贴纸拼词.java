775ec055acfdbc97b55a5228a3a8dfb95b3afe6e
houyuanbo <houyuanbo@qianxin.com>
2022-05-14 17:02:35 +0800

    力扣刷题0514

---
+++ src/main/java/com/haijiao12138/demo/leetcode/code2022/day_0514/力扣_691_贴纸拼词.java
@@ -0,0 +1,15 @@
+package com.haijiao12138.demo.leetcode.code2022.day_0514;
+
+/**
+ * @ClassName 力扣_691_贴纸拼词
+ * @Author houyuanbo
+ * @Date 2022/5/14 16:37
+ * @Description TODO
+ * @Version
+ **/
+public class 力扣_691_贴纸拼词 {
+    public static int minStickers(String[] stickers, String target) {
+
+        return 1;
+    }
+}
